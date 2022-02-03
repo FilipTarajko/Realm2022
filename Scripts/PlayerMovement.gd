28f108c980555aef126b1a196936a5c4397520f7
Filip Tarajko <filiptarajko@gmail.com>
2022-02-03 22:59:02 +0100

    add scaling, sprite and modulate to weapons

--- Scripts/PlayerMovement.gd
+++ Scripts/PlayerMovement.gd
@@ -5,37 +5,80 @@ var move_directon = Vector2(0, 0)
 
 var can_fire = true
 
+var bulletSprites = {
+	"sword": preload("res://assets/bulletSprites/swordBullet.png"),
+}
+
 var weapons = {
 	"bow": {
 		0: {
 			"att_spd": 3.5,
-			"projectile_speed": 150,
+			"projectile_speed": 200,
 			"lifetime": 0.25,
-			"shots": 1
+			"shots": 1,
+			"angle": 0,
+			"scalex": 0.5,
+			"scaley": 0.5,
+			"modulate": Color(1, 1, 1),
 		},
 		1: {
-			"att_spd": 5.5,
+			"att_spd": 4.5,
 			"projectile_speed": 250,
 			"lifetime": 0.25,
 			"shots": 2,
+			"angle": 15,
+			"scalex": 0.5,
+			"scaley": 0.5,
+			"modulate": Color(1, 0, 0),
 		},
 		2: {
 			"att_spd": 5.5,
-			"projectile_speed": 350,
-			"lifetime": 0.2,
-			"shots": 3
+			"projectile_speed": 250,
+			"lifetime": 0.28,
+			"shots": 3,
+			"angle": 30,
+			"scalex": 0.5,
+			"scaley": 0.5,
+			"modulate": Color(0, 0, 0),
+		},
+		3: {
+			"att_spd": 6.5,
+			"projectile_speed": 250,
+			"lifetime": 0.28,
+			"shots": 5,
+			"angle": 30,
+			"scalex": 0.5,
+			"scaley": 0.5,
+			"modulate": Color(0, 0, 1),
 		},
 	},
 	"sword": {
 		0: {
-			"att_spd": 16.5,
+			"att_spd": 3.5,
+			"projectile_speed": 200,
+			"lifetime": 0.15,
+			"shots": 1,
+			"angle": 0,
+			"scalex": 0.4,
+			"scaley": 0.8,
+			"sprite": bulletSprites["sword"],
+			"modulate": Color(1, 1, 1),
+		},
+		1: {
+			"att_spd": 5.5,
 			"projectile_speed": 250,
-			"lifetime": 0.1
+			"lifetime": 0.15,
+			"shots": 2,
+			"angle": 10,
+			"scalex": 0.7,
+			"scaley": 0.5,
+			"sprite": bulletSprites["sword"],
+			"modulate": Color(0, 0.5, 1),
 		}
 	}
 }
 
-var usedWeapon = weapons["bow"][2]
+var usedWeapon = weapons["sword"][1]
 
 func _physics_process(delta):
 	handleMovement()
@@ -75,12 +118,19 @@ func handleAnimation():
 func handleShooting():
 	if Input.is_action_pressed("Shoot") and can_fire == true:
 		can_fire = false
-		var new_arrow = arrowPrefab.instance()
-		new_arrow.position = get_global_position()
-		new_arrow.rotation = get_angle_to(get_global_mouse_position())+PI/2
-		new_arrow.projectile_speed = usedWeapon.projectile_speed
-		new_arrow.lifetime = usedWeapon.lifetime
-		get_parent().add_child(new_arrow)
-		print(Engine.get_frames_per_second())
+		for i in range(usedWeapon.shots):
+			var new_arrow = arrowPrefab.instance()
+			new_arrow.position = get_global_position()
+			new_arrow.projectile_speed = usedWeapon.projectile_speed
+			new_arrow.lifetime = usedWeapon.lifetime
+			print(i)
+			new_arrow.rotation = get_angle_to(get_global_mouse_position())+PI/2+deg2rad((i-((usedWeapon.shots-1)/2))*((usedWeapon.angle)/(usedWeapon.shots)))
+			print("{%s}: {%s}" % [i, new_arrow.rotation])
+			new_arrow.get_child(1).texture = usedWeapon["sprite"]
+			new_arrow.modulate = usedWeapon.modulate
+			new_arrow.scale.x = usedWeapon.scalex
+			new_arrow.scale.y = usedWeapon.scaley
+			get_parent().add_child(new_arrow)
+		#print(Engine.get_frames_per_second())
 		yield(get_tree().create_timer(1/usedWeapon.att_spd), "timeout")
 		can_fire = true
