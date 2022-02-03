502bb31c4e0763a5a7c5f278950a1b93face241f
Filip Tarajko <filiptarajko@gmail.com>
2022-02-03 19:11:46 +0100

    add directions of player sprite, add shadow

--- PlayerMovement.gd
+++ PlayerMovement.gd
@@ -4,15 +4,32 @@ var speed = 40
 var move_directon = Vector2(0, 0)
 
 func _physics_process(delta):
-	MovementLoop()
+	HandleMovement()
 
 func _process(delta):
-	pass
+	HandleAnimation()
 
+var textureDown = preload("res://archerGreen.png")
+var textureUp = preload("res://archerGreenBack.png")
+var textureLeft = preload("res://archerGreenLeft.png")
+var textureRight = preload("res://archerGreenRight.png")
 
-func MovementLoop():
+onready var sprite_node = get_node("Sprite")
+
+func HandleMovement():
 	move_directon.x = int(Input.is_action_pressed("Right")) - int (Input.is_action_pressed("Left"))
 	move_directon.y = int(Input.is_action_pressed("Down")) - int (Input.is_action_pressed("Up"))
 	var movement = move_directon.normalized()*speed
 	# move_and_slide has delta built-in
 	move_and_slide(movement)
+
+
+func HandleAnimation():
+	if Input.is_action_pressed("Down"):
+		sprite_node.texture=textureDown
+	elif Input.is_action_pressed("Up"):
+		sprite_node.texture = textureUp
+	elif Input.is_action_pressed("Right"):
+		sprite_node.texture = textureRight
+	elif Input.is_action_pressed("Left"):
+		sprite_node.texture = textureLeft
